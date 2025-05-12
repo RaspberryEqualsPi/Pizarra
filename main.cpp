dbc63d09456647fc7c6cd5bb2d870934d5ae46a4
Gregory Yeghiyan <46092559+RaspberryEqualsPi@users.noreply.github.com>
2025-05-11 22:01:41 -0400

    basic whiteboard functionality implemented

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+#include <iostream>
 #include <vector>
 #include <cmath>
 
@@ -31,36 +34,155 @@ std::vector<sf::Vertex> createThickLine(sf::Vector2f a, sf::Vector2f b, float th
     };
 }
 
+class CircleButton : public tgui::CanvasSFML {
+public:
+    using Ptr = std::shared_ptr<CircleButton>;
+
+    CircleButton(float radius, sf::Color normalColor = sf::Color::Blue, sf::Color hoverColor = sf::Color::Green)
+        : m_radius(radius), m_normalColor(normalColor), m_hoverColor(hoverColor), m_isHovered(false)
+    {
+        setSize({radius * 2, radius * 2});
+        updateGraphics();
+    }
+
+    static Ptr create(float radius, sf::Color normalColor = sf::Color::Blue, sf::Color hoverColor = sf::Color::Green) {
+        return std::make_shared<CircleButton>(radius, normalColor, hoverColor);
+    }
+
+    void setPosition(sf::Vector2f pos) {
+        tgui::CanvasSFML::setPosition(tgui::Layout2d(pos));
+    }
+
+    sf::Color getNormalColor(){
+        return m_normalColor;
+    }
+
+    sf::Color getHoverColor(){
+        return m_hoverColor;
+    }
+
+    void setOutline(sf::Color outlineColor, float outlineThickness){
+        m_outlineColor = outlineColor;
+        m_outlineThickness = outlineThickness;
+        setSize({(m_radius + outlineThickness) * 2, (m_radius + outlineThickness) * 2});
+        setPosition({getPosition().x - m_outlineThickness + m_totalOffset, getPosition().y - m_outlineThickness + m_totalOffset}); // totalOffset term for correction
+        m_totalOffset = m_outlineThickness;
+        updateGraphics();
+    }
+
+    tgui::Signal& getSignal(tgui::String signalName) override {
+        if (signalName == "Clicked")
+            return m_signalClicked;
+        return tgui::CanvasSFML::getSignal(signalName);
+    }
+
+protected:
+    bool mouseOnWidget(tgui::Vector2f pos) const {
+        float dx = pos.x - m_radius;
+        float dy = pos.y - m_radius;
+        return (dx * dx + dy * dy) <= (m_radius * m_radius);
+    }
+
+    void mouseEnteredWidget() override {
+        m_isHovered = true;
+        updateGraphics();
+    }
+
+    void mouseLeftWidget() override {
+        m_isHovered = false;
+        updateGraphics();
+    }
+
+    bool leftMousePressed(tgui::Vector2f) {
+        return m_signalClicked.emit(this);
+    }
+
+private:
+    void updateGraphics() {
+        clear(sf::Color::Transparent);
+        sf::CircleShape circle(m_radius);
+        circle.setPointCount(100); // Smooth circle
+        circle.setFillColor(m_isHovered ? m_hoverColor : m_normalColor);
+        circle.setPosition({m_outlineThickness, m_outlineThickness});
+        circle.setOutlineThickness(m_outlineThickness);
+        circle.setOutlineColor(m_outlineColor);
+        draw(circle);
+        display();
+    }
+
+    float m_radius;
+    float m_outlineThickness = 0;
+    bool m_isHovered;
+    sf::Color m_outlineColor;
+    sf::Color m_normalColor;
+    sf::Color m_hoverColor;
+    float m_totalOffset = 0;
+    tgui::Signal m_signalClicked{"Clicked"};
+};
+
 int main() {
     const unsigned int windowWidth = 800;
     const unsigned int windowHeight = 600;
     const float spacing = 5.0f;
     const float lineThickness = 10.0f;
-
-    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "Pizarra");
+    sf::Color strokeColor = sf::Color::Black;
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "Pizarra", sf::Style::Default, sf::State::Windowed, settings);
+    std::pair<sf::Vector2i, sf::Vector2i> boardBounds = {{0, 0}, {windowWidth, windowHeight - 100}};
     window.setFramerateLimit(120);
 
-    std::vector<std::vector<sf::Vector2f>> allStrokePoints;
+
+    std::vector<std::pair<std::vector<sf::Vector2f>, sf::Color>> allStrokePoints;
     std::vector<sf::Vector2f> currentStroke;
     bool drawing = false;
     sf::Vector2f lastPos;
+    tgui::Gui gui{window};
 
+    auto brushPanel = tgui::Panel::create({windowWidth, 100});
+    brushPanel->setPosition({0, windowHeight - 100});
+    // Set background color via renderer
+    brushPanel->getRenderer()->setBackgroundColor(tgui::Color(32, 32, 32)); 
+
+    sf::Color brushColors[] = {sf::Color::Black, sf::Color::White, sf::Color::Red, sf::Color::Yellow, sf::Color::Green, sf::Color::Blue, sf::Color::Magenta};
+    CircleButton::Ptr brushButtons[sizeof(brushColors)/sizeof(sf::Color)];
+    gui.add(brushPanel);
+    for (int i = 0; i < sizeof(brushColors)/sizeof(sf::Color); i++){
+        auto button = CircleButton::create(20, brushColors[i], brushColors[i]);
+        button->setPosition({windowWidth - 50*sizeof(brushColors)/sizeof(sf::Color) - 100 + i*50.f, (float)windowHeight - 70.f});
+        button->getSignal("Clicked").connect([&brushButtons, i, brushColors, &strokeColor](){
+            for (int j = 0; j < sizeof(brushColors)/sizeof(sf::Color); j++){
+                brushButtons[j]->getSize();
+                brushButtons[j]->setOutline(sf::Color::White, 0);
+            }
+            brushButtons[i]->setOutline(sf::Color::White, 3);
+            strokeColor = brushButtons[i]->getNormalColor();
+        });
+        brushButtons[i] = button;
+        gui.add(button);
+    } 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
+            gui.handleEvent(*event);
             if (event->is<sf::Event::Closed>())
                 window.close();
 
             if (event->is<sf::Event::MouseButtonPressed>() && event->getIf<sf::Event::MouseButtonPressed>()->button == sf::Mouse::Button::Left) {
-                drawing = true;
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                if (mousePos.x >= boardBounds.first.x && mousePos.x <= boardBounds.second.x && mousePos.y >= boardBounds.first.y && mousePos.y <= boardBounds.second.y){
+                    drawing = true;
+                } else {
+                    drawing = false;
+                }
                 currentStroke.clear();
-                lastPos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                lastPos = mousePos;
                 currentStroke.push_back(lastPos);
             }
 
             if (event->is<sf::Event::MouseButtonReleased>() && event->getIf<sf::Event::MouseButtonReleased>()->button == sf::Mouse::Button::Left) {
                 drawing = false;
                 if (currentStroke.size() >= 2) {
-                    allStrokePoints.push_back(currentStroke);
+                    allStrokePoints.push_back(std::pair<std::vector<sf::Vector2f>, sf::Color>(currentStroke, strokeColor));
                 }
             }
 
@@ -69,9 +191,12 @@ int main() {
                 currentStroke.clear();
             }
         }
-
+        
         if (drawing) {
             sf::Vector2f currentPos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+            if (!(currentPos.x >= boardBounds.first.x && currentPos.x <= boardBounds.second.x && currentPos.y >= boardBounds.first.y && currentPos.y <= boardBounds.second.y)){
+                drawing = false;
+            }
             float dist = std::hypot(currentPos.x - lastPos.x, currentPos.y - lastPos.y);
             int steps = std::max(1, static_cast<int>(dist / spacing));
 
@@ -88,18 +213,18 @@ int main() {
 
         // Draw all finished strokes
         for (const auto& stroke : allStrokePoints) {
-            for (size_t i = 1; i < stroke.size(); ++i) {
-                std::vector<sf::Vertex> tri = createThickLine(stroke[i - 1], stroke[i], lineThickness, sf::Color::Black);
+            for (size_t i = 1; i < stroke.first.size(); ++i) {
+                std::vector<sf::Vertex> tri = createThickLine(stroke.first[i - 1], stroke.first[i], lineThickness, stroke.second);
                 window.draw(tri.data(), tri.size(), sf::PrimitiveType::Triangles);
             }
         }
 
         // Draw current stroke in progress
         for (size_t i = 1; i < currentStroke.size(); ++i) {
-            std::vector<sf::Vertex> tri = createThickLine(currentStroke[i - 1], currentStroke[i], lineThickness, sf::Color::Black);
+            std::vector<sf::Vertex> tri = createThickLine(currentStroke[i - 1], currentStroke[i], lineThickness, strokeColor);
             window.draw(tri.data(), tri.size(), sf::PrimitiveType::Triangles);
         }
-
+        gui.draw();
         window.display();
     }
 
